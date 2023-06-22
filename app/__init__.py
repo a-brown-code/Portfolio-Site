174a92dd39f12ce6b0830530c7ee09981196fd13
MAMV3x3 <mamv3x3@gmail.com>
2023-06-22 15:51:24 -0500

    Added JSON data file for locations, fixed map display CSS and added responsive support

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import folium
+import json
 
 load_dotenv()
 app = Flask(__name__)
@@ -9,8 +10,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    m = folium.Map()
+    # Create a map object
+    m = folium.Map(min_zoom=2)
+
+    # Get JSON data path
+    locations_file_path = os.path.join(os.path.dirname(__file__), 'static', 'data', 'locations.json')
+
+    # Load member locations from JSON file)
+    with open(locations_file_path, 'r') as f:
+        data = json.load(f)
+        members = data['members']
+        for member in members:
+            color = member['color']
+            locations = member['locations']
+            for location in locations:
+                lat = location['lat']
+                lon = location['lon']
+                location_name = location['name']
+                location_desctiption = location['description']
+                marker = folium.Marker(location=[lat, lon], icon=folium.Icon(color=color, icon='star'), popup=location_desctiption, tooltip=location_name)
+                marker.add_to(m)
+    
+    # Set map properties
     m.get_root().width = '100%'
     m.get_root().height = '100%'
-    iframe = m.get_root()._repr_html_()
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), iframe=iframe)
+    
+    # Generate the HTML embed code
+    map = m.get_root()._repr_html_()
+
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), map=map, members=members)
