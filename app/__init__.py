df8c6fd2f90a3b3e132b89e76fcedbd8bad97235
MAMV3x3 <mamv3x3@gmail.com>
2023-06-22 03:42:27 -0500

    Added map support using folium

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+import folium
 
 load_dotenv()
 app = Flask(__name__)
@@ -8,4 +9,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    m = folium.Map()
+    m.get_root().width = '100%'
+    m.get_root().height = '100%'
+    iframe = m.get_root()._repr_html_()
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), iframe=iframe)
