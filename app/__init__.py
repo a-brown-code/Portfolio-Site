d1ea90f8c5b9ce3cf99b49f87e96dce213641403
MAMV3x3 <mamv3x3@gmail.com>
2023-06-23 02:17:12 -0500

    Refactor: separation of logic into smaller and modular methods
    - Separated the marker creation logic into the create_marker method
    - Created the create_map method to generate the map with markers
    - Implemented the load_member_locations method to load member location data from the JSON file
    - Updated index function to use the new methods and improve code readability

--- app/__init__.py
+++ app/__init__.py
@@ -3,41 +3,63 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import folium
 import json
+from pathlib import Path
 
 load_dotenv()
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    # Create a map object
+def create_marker(location, color):
+    marker = folium.Marker(
+        location=[location['lat'], location['lon']],
+        icon=folium.Icon(color=color, icon='star'),
+        popup=folium.Popup(
+            folium.IFrame(
+                f'''
+                <style>
+                    h3 {{font-family: "Roboto", serif; text-align: center;}}
+                    p {{font-family: "Roboto", serif; text-align: center;}}
+                </style>
+                <h3>{location['name']}</h3>
+                <p>{location['description']}</p>
+                ''',
+                width=200,
+                height=100
+            )
+        ),
+        tooltip=location['name']
+    )
+    return marker
+
+
+def create_map(members):
     m = folium.Map(min_zoom=2)
 
-    # Get JSON data path
-    locations_file_path = os.path.join(os.path.dirname(__file__), 'static', 'data', 'locations.json')
+    for member in members:
+        color = member['color']
 
-    # Load member locations from JSON file)
+        for location in member['locations']:
+            marker = create_marker(location, color)
+            marker.add_to(m)
+
+    m.get_root().width = '100%'
+    m.get_root().height = '100%'
+    map_html = m.get_root()._repr_html_()
+    return map_html
+
+
+def load_member_locations():
+    locations_file_path = Path(__file__).resolve(
+    ).parent / 'static' / 'data' / 'locations.json'
     with open(locations_file_path, 'r') as f:
         data = json.load(f)
         members = data['members']
-        for member in members:
-            color = member['color']
-            locations = member['locations']
-            for location in locations:
-                lat = location['lat']
-                lon = location['lon']
-                location_name = location['name']
-                location_description = location['description']
-                popup_html = f'<style>h3 {{font-family: "Roboto", serif; text-align: center;}} p {{font-family: "Roboto", serif; text-align: center;}}</style> <h3>{location_name}</h3><p>{location_description}</p>'
-                iframe = folium.IFrame(html=popup_html, width=250, height=100)
-                marker = folium.Marker(location=[lat, lon], icon=folium.Icon(color=color, icon='star'), popup=folium.Popup(iframe), tooltip=location_name)
-                marker.add_to(m)
-    
-    # Set map properties
-    m.get_root().width = '100%'
-    m.get_root().height = '100%'
-    
-    # Generate the HTML embed code
-    map = m.get_root()._repr_html_()
+    return members
 
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), map=map, members=members)
+
+@app.route('/')
+def index():
+    members = load_member_locations()
+    map_html = create_map(members)
+
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), map=map_html, members=members)
