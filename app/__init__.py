228c661d2a75db85f179fdc95c554cf8d8f38587
MAMV3x3 <mamv3x3@gmail.com>
2023-06-22 22:00:58 -0500

    Added JSON data support and made tab selector for every fellow

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 app = Flask(__name__)
@@ -8,4 +9,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    # Get JSON data path
+    education_file_path = os.path.join(os.path.dirname(__file__), 'static', 'data', 'education.json')
+
+    with open(education_file_path, 'r') as f:
+        members_education_data = json.load(f)
+
+    members_education = members_education_data['members']
+
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), members_education=members_education)
