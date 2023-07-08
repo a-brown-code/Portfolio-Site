7937e4328bdbcf26c557a538bcc9fee354ec651d
Aerin Brown <72281505+a-brown-code@users.noreply.github.com>
2023-07-08 14:18:47 -0700

    fix automated redeployment

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 tmux kill-server
+rm -r -f python3-virtualenv
 git fetch && git reset origin/main --hard
-tmux new
+tmux -d new
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
+pip install --upgrade
 pip install -r requirements.txt
 if [[ $? -ne 0 ]]
 then
-REQS=$( grep -o "[[:alnum:]]*=" requirements.txt | sed 's/=//' )
+REQS=$( grep -o "[a-zA-Z0-9\-]*=" requirements.txt | sed 's/=//' )
 for REQ in $REQS
 do
 pip install $REQ
