00c9bd547963ce7307ff43ba420e44973137e43d
Aerin Brown <72281505+a-brown-code@users.noreply.github.com>
2023-07-07 21:19:10 -0700

    feat: automated deployment

---
+++ redeploy-site.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+tmux kill-server
+git fetch && git reset origin/main --hard
+tmux new
+python -m venv python3-virtualenv
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+if [ $? -ne 0 ]
+then
+REQS=$( grep -o "[[:alnum:]]*=" requirements.txt | sed 's/=//' )
+for REQ in $REQS
+do
+pip install $REQ
+done
+fi
+export FLASK_ENV=development
+flask run --host=0.0.0.0
