e48150ce6399086d8de10e9cf6e5625d1d729ea7
Aerin Brown <aerinebrown04@gmail.com>
2023-07-14 00:53:33 -0700

    feat: updated redeploy-site to work with service

--- test-scripts/redeploy-site.sh
+++ test-scripts/redeploy-site.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-tmux kill-server
+cd ..
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
@@ -13,6 +13,4 @@ do
 pip install $REQ
 done
 fi
-tmux new -d
-tmux send-keys 'export FLASK_ENV=development' C-m
-tmux send-keys 'flask run --host=0.0.0.0' C-m
+systemctl restart myportfolio
