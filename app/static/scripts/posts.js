f401568728fe4c5095777e6ef4830dbdeaf1c3c4
Aerin Brown <aerinebrown04@gmail.com>
2023-07-28 12:19:39 -0700

    fix: typo

--- app/static/scripts/posts.js
+++ app/static/scripts/posts.js
@@ -3,13 +3,13 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
     const formData = new FormData(form);
     const payload = new URLSearchParams(formData);
-    fetch('http://http://aerinbrownportfolio.duckdns.org:5000//api/timeline_post', {method: 'POST', body: payload})
+    fetch('http://http://aerinbrownportfolio.duckdns.org:5000/api/timeline_post', {method: 'POST', body: payload})
     .then(res => res.json())
     .then(data => console.log(data))
     location.reload();
 })
 
-fetch('http://http://aerinbrownportfolio.duckdns.org:5000//api/timeline_post')
+fetch('http://http://aerinbrownportfolio.duckdns.org:5000/api/timeline_post')
     .then((response) => {return response.json();})
     .then((data) => {
         let posts = data.timeline_posts;
