cd53dc491fe194742dbe7786b0e34760c36b67ec
Aerin Brown <aerinebrown04@gmail.com>
2023-07-28 13:27:17 -0700

    feat: updated redeploy-site for docker

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 cd ..
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-if [[ $? -ne 0 ]]
-then
-REQS=$( grep -o "[a-zA-Z0-9\-]*=" requirements.txt | sed 's/=//' )
-for REQ in $REQS
-do
-pip install $REQ
-done
-fi
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
