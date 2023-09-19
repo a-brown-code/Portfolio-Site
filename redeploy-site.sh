ea2e754819fcdaa1b4379e6164b890769c52cd48
Aerin Brown <72281505+aerin-brown-codes@users.noreply.github.com>
2023-09-19 12:43:29 -0400

    fix: added root privileges to use docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git fetch && git reset origin/main --hard
-docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up -d --build
+sudo docker compose -f docker-compose.prod.yml down
+sudo docker compose -f docker-compose.prod.yml up -d --build
